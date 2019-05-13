971c0d66662dd1163a7c3276feada8478d68cfc7
CarlosUni <100074907@alumnos.uc3m.es>
2019-05-13 22:10:18 +0200

    Modificado a BigInteger
    Modificado todos los ints a BigInteger (revisar los no necesarios). Funciona todo, incluida entrada de parametros

--- src/rsa/EncryptDecryptRSA.java
+++ src/rsa/EncryptDecryptRSA.java
@@ -19,43 +19,57 @@ class EncryptDecryptRSA{
 	public static void main(String args[]) {
 		if (args.length == 3) {
 			//Scanner sc = new Scanner(System.in);
-			int p, q, n, z, d = 0, e, i;
+			BigInteger p = new BigInteger("0");
+			BigInteger q = new BigInteger("0");
+			BigInteger n = new BigInteger("0");
+			BigInteger z = new BigInteger("0");
+			BigInteger d = new BigInteger("0");
+			BigInteger e;
+			//BigInteger i;
+			BigInteger unitBigInt = new BigInteger("1");
+			BigInteger zeroBigInt = new BigInteger("0");
+
 			//int msg = sc.nextInt();
-			int msg = Integer.parseInt(args[0]);
+			BigInteger msg = new BigInteger(args[0]);
 			System.out.println("El mensaje (numero) introducido es = " + msg);
-			double c;
+			BigInteger c;
 			BigInteger msgback; 
-			p =  Integer.parseInt(args[1]);
+			p =  new BigInteger(args[1]);
 			System.out.println("El primer primo p introducido es = " + p);
-			q =  Integer.parseInt(args[2]);
-			System.out.println("El primer primo p introducido es = " + q);
+			q =  new BigInteger(args[2]);
+			System.out.println("El segundo primo q introducido es = " + q);
 			
-			n = p * q;
-			z = (p - 1) * (q - 1);
+			n = p.multiply(q);
+			z = p.subtract(unitBigInt).multiply(q.subtract(unitBigInt));
 			System.out.println("El valor de  z es = " + z);		
 
-			for (e = 2; e < z; e++) {
-				if (gcd(e, z) == 1) {	          // e is for public key exponent
+			for (e = new BigInteger("2"); e.compareTo(z) == -1; e = e.add(unitBigInt)) {
+				if (gcd(e, z, zeroBigInt).compareTo(unitBigInt) == 0) {	          // e is for public key exponent
 					break;
 				}
 			}
-			System.out.println("El valor de es es = " + e);				
-			for (i = 0; i <= 9; i++) {
-				int x = 1 + (i * z);
-				if(x % e == 0) {      //d is for private key exponent
-					d = x / e;
+			
+			System.out.println("El valor de es es = " + e);	
+			
+			for (BigInteger i = new BigInteger("0");
+					(i.compareTo(new BigInteger("9")) == 0) || (i.compareTo(new BigInteger("9")) == -1);
+					i = i.add(unitBigInt)) {
+				BigInteger x = unitBigInt.add(i.multiply(z));
+				
+				if (x.mod(e).compareTo(zeroBigInt) == 0) {      //d is for private key exponent
+					d = x.divide(e);
 					break;
 				}
 			}
 			System.out.println("El valor de d es = " + d);		
-			c = (Math.pow(msg, e)) % n;
+			c = msg.modPow(e, n);
 			System.out.println("El mensaje encriptado es = " + c);
 			//System.out.println(c);
 	        // Converting int value of n to BigInteger
-			BigInteger N = BigInteger.valueOf(n);
+			//BigInteger N = BigInteger.valueOf(n);
 			// Converting float value of c to BigInteger
-			BigInteger C = BigDecimal.valueOf(c).toBigInteger();
-			msgback = (C.pow(d)).mod(N);
+			//BigInteger C = BigDecimal.valueOf(c).toBigInteger();
+			msgback = (c.modPow(d, n));
 			System.out.println("EL mensaje desencriptado es = " + msgback);
 			//System.out.println(msgback);
 		} else {
@@ -64,11 +78,11 @@ class EncryptDecryptRSA{
 		
 	}
 
-	static int gcd(int e, int z) {
-		if(e == 0) {
+	static BigInteger gcd(BigInteger e, BigInteger z, BigInteger zeroBigInt) {
+		if (e.compareTo(zeroBigInt) == 0) {
 			return z;
 		} else {
-			return gcd(z % e,e);
+			return gcd(z.mod(e),e, zeroBigInt);
 		}
 	}
 }
